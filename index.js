612720c21a883182841a3357030616657f101854
Max Peterson <max.peterson@apps.ask.com>
2016-11-03 11:06:28 -0400

    Adds initial setup for botkit with "hello" test response

---
+++ index.js
@@ -0,0 +1,21 @@
+if (!process.env.BOT_TOKEN) {
+  console.error('**ERROR**');
+  console.error('Please add your bot user token to your env vars with the name "BOT_TOKEN"');
+  process.exit(1);
+}
+const Botkit = require('botkit');
+const controller = Botkit.slackbot();
+const bot = controller.spawn({
+  token: process.env.BOT_TOKEN
+});
+
+controller.hears('hello', ['direct_message', 'direct_mention'], (bot, message) => {
+  bot.reply(message, 'hello!');
+});
+
+bot.startRTM((err, bot, payload) => {
+  if (err) {
+    console.error(err);
+    throw new Error('Could not connect to Slack');
+  }
+});

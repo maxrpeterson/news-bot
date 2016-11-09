8bfb1a15d40fb41de067081f5257bf4373a8eec5
Max Peterson <max.peterson@apps.ask.com>
2016-11-09 17:19:35 -0500

    Moves controller to its own module
    Allows a way to get the same instance of the controller in multiple other
    modules

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ if (!process.env.BOT_TOKEN) {
   console.error('Please add your bot user token to your env vars with the name "BOT_TOKEN"');
   process.exit(1);
 }
-const Botkit = require('botkit');
-const controller = Botkit.slackbot({
-  json_file_store: './db'
-});
+const controller = require('./lib/controller');
 const bot = controller.spawn({
   token: process.env.BOT_TOKEN
 });
+
+const scheduler = require('./lib/scheduler');
+scheduler.init(controller);
 
 const newsNow = require('./lib/news-now');
 newsNow.init(controller);
@@ -17,9 +17,6 @@ newsNow.init(controller);
 controller.hears('hello', ['direct_message', 'direct_mention'], (bot, message) => {
   bot.reply(message, 'hello!');
 });
-
-const scheduler = require('./lib/scheduler');
-scheduler.start(controller);
 
 bot.startRTM((err, bot, payload) => {
   if (err) {
